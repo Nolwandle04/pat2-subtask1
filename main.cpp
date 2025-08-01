b077115acca2f744f0435956d30b240e0d51db4a
Lwandle Magwanyana <lwandlemagwanyana@gmail.com>
2025-08-01 11:27:48 +0200

    Final version with full Morse code converter

--- main.cpp
+++ main.cpp
@@ -1,42 +1,37 @@
 #include <iostream>
 #include <map>
-using namespace std;
-
-int main() {
-    map<char, string> morseCode = {
-        {'A', ".-"}, {'B', "-..."}, {'C', "-.-."}, {'D', "-.."},
-        {'E', "."}, {'F', "..-."}, {'G', "--."}, {'H', "...."},
-        {'I', ".."}, {'J', ".---"}, {'K', "-.-"}, {'L', ".-.."},
-        {'M', "--"}, {'N', "-."}, {'O', "---"}, {'P', ".--."},
-        {'Q', "--.-"}, {'R', ".-."}, {'S', "..."}, {'T', "-"},
-        {'U', "..-"}, {'V', "...-"}, {'W', ".--"}, {'X', "-..-"},
-        {'Y', "-.--"}, {'Z', "--.."}, {' ', "/"}
-    };
-    return 0;
-}
-#include <iostream>
-#include <map>
 #include <string>
+#include <cctype>
+
 using namespace std;
 
 int main() {
-    map<char, string> morseCode = { /* same as above */ };
+    // Morse Code Map for A–Z and space
+    map<char, string> morseCode = {
+        {'A', ".-"},    {'B', "-..."},  {'C', "-.-."},  {'D', "-.."},
+        {'E', "."},     {'F', "..-."},  {'G', "--."},   {'H', "...."},
+        {'I', ".."},    {'J', ".---"},  {'K', "-.-"},   {'L', ".-.."},
+        {'M', "--"},    {'N', "-."},    {'O', "---"},   {'P', ".--."},
+        {'Q', "--.-"},  {'R', ".-."},   {'S', "..."},   {'T', "-"},
+        {'U', "..-"},   {'V', "...-"},  {'W', ".--"},   {'X', "-..-"},
+        {'Y', "-.--"},  {'Z', "--.."},
+        {' ', "/"} // Space between words
+    };
 
     string input;
     cout << "Enter a message to convert to Morse code: ";
     getline(cin, input);
 
-    return 0;
-}
-for (char c : input) {
-    c = toupper(c);
-}
-cout << "\nMorse Code:\n";
-for (char c : input) {
-    c = toupper(c);
-    if (morseCode.find(c) != morseCode.end()) {
-        cout << morseCode[c] << " ";
-    } else {
-        cout << "? ";
+    cout << "\nMorse Code:\n";
+    for (char c : input) {
+        c = toupper(c); // Convert character to uppercase
+        if (morseCode.find(c) != morseCode.end()) {
+            cout << morseCode[c] << " ";
+        } else {
+            cout << "? "; // Unknown character
+        }
     }
+
+    cout << endl;
+    return 0;
 }
