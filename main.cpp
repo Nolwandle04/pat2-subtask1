2784d0b0542dd1451cd37cb26c764f21def9d459
Lwandle Magwanyana <lwandlemagwanyana@gmail.com>
2025-08-01 11:25:46 +0200

    Add user input prompt and capture message

--- main.cpp
+++ main.cpp
@@ -14,3 +14,17 @@ int main() {
     };
     return 0;
 }
+#include <iostream>
+#include <map>
+#include <string>
+using namespace std;
+
+int main() {
+    map<char, string> morseCode = { /* same as above */ };
+
+    string input;
+    cout << "Enter a message to convert to Morse code: ";
+    getline(cin, input);
+
+    return 0;
+}
